73c0daa8e77515f69e4c761214059eacf66f2d51
Rajmani Kushwaha <rajmanikush@gmail.com>
2024-05-10 09:03:36 +0530

    Initial Commit

---
+++ TalkShopApp/TalkShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkShopAppApp.swift
+//  TalkShopApp
+//
+//  Created by Rajmani Kushwaha on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TalkShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
