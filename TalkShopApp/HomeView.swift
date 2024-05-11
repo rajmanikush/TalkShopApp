7e78a282fc2edb51c094adabb778286d9c39dff0
Rajmani Kushwaha <rajmanikush@gmail.com>
2024-05-11 17:30:24 +0530

    add tab view

---
+++ TalkShopApp/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TalkShopApp
+//
+//  Created by Rajmani Kushwaha on 10/05/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        TabView {
+            FeedMainView()
+                .tabItem { Label("Feed", systemImage: "house.fill") }
+            
+            AccountView()
+                    .tabItem {
+                        Label("Profile", systemImage: "person.crop.circle.fill")
+                    }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
--- TalkShopApp/TalkShopApp.swift
+++ TalkShopApp/TalkShopApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct TalkShopAppApp: App {
+struct TalkShopApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
